65745a25e62d6d0ed57c1969f5b0137b4c554319
mikaeldiok <mikaeldiok@gmail.com>
2022-11-07 16:11:43 +0700

    curl timeout span

--- src/Comms/Whatsapp.php
+++ src/Comms/Whatsapp.php
@@ -109,7 +109,7 @@ class Whatsapp implements WhatsappInterface
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_ENCODING => "",
                 CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 30,
+                CURLOPT_TIMEOUT => 30000,
                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                 CURLOPT_CUSTOMREQUEST => "GET",
             ));
